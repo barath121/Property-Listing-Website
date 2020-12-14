78937b85851525dce7b829fd411874a17b02ea28
Terry-atb <mohammed.aftab18@siesgst.ac.in>
2020-12-14 21:19:38 +0530

    initial commit

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ Routesinit(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`App running on port http://localhost:${port}`);
 });
