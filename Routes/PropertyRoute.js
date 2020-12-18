9e5fdfd66f5ef51a50091a8265bb4dfddad828ac
Barath Nadar <nadar.barath18@siesgst.ac.in>
2020-12-19 03:17:27 +0530

    Revert "Revert "Image Upload COmpleted""
    This reverts commit 026760870dedbce54c595c29bc8bc578c3b67a49.

--- Routes/PropertyRoute.js
+++ Routes/PropertyRoute.js
@@ -2,6 +2,10 @@ const Property = require("../Models/Property");
 const express = require('express');
 const PropertyController = require('../Controller/PropertyController');
 const PropertyRoute = express.Router();
+var multer = require('multer');
+var upload = multer({
+    storage : multer.memoryStorage()
+});
 PropertyRoute.route('/test').get(PropertyController.test);
-
+PropertyRoute.post('/createproperty',upload.any(),PropertyController.createProperty);
 module.exports = PropertyRoute;
