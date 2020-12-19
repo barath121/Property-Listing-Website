4f578b95a50134461c46af6b088cbfabdde4e4b0
sasukeuzumaki31 <nimeshnairns@gmail.com>
2020-12-19 16:29:18 +0530

    Admin Form updated 3.0

--- Routes/PropertyRoute.js
+++ Routes/PropertyRoute.js
@@ -3,5 +3,10 @@ const express = require('express');
 const PropertyController = require('../Controller/PropertyController');
 const PropertyRoute = express.Router();
 PropertyRoute.route('/test').get(PropertyController.test);
+PropertyRoute.route('/test2').get((req,res)=>{
+    res.render('Create_property_2')
+})
+
+
 
 module.exports = PropertyRoute;
