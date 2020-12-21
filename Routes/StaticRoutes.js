3ac14c9d6257eb9cbb320355699a147e6f3e3387
Terry-atb <mohammed.aftab18@siesgst.ac.in>
2020-12-21 17:24:52 +0530

    login register page added

--- Routes/StaticRoutes.js
+++ Routes/StaticRoutes.js
@@ -2,6 +2,12 @@ const StaticRoutes = (app) =>{
     app.get('/', (req, res) => {
         res.render('index')
     })
+    app.get('/login', (req, res) => {
+        res.render('login')
+    })
+    app.get('/register', (req, res) => {
+        res.render('register')
+    })
     app.get('/contact', (req, res) => {
         res.render('contact')
     })
