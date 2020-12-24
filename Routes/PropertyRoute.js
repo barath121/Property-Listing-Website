2231ef5a1e62659443701896155ef205611e182d
Barath Nadar <nadar.barath18@siesgst.ac.in>
2020-12-24 20:11:26 +0530

    Added Names To images , Firebase Upload Folder System Added

--- Routes/PropertyRoute.js
+++ Routes/PropertyRoute.js
@@ -13,4 +13,6 @@ PropertyRoute.route('/test2').get((req,res)=>{
 
 
 PropertyRoute.post('/createproperty',upload.any(),PropertyController.createProperty);
+PropertyRoute.post('/commercialproperty',upload.any(),PropertyController.CommercialProperty);
+
 module.exports = PropertyRoute;
