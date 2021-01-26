9888ffc60fc6c4141d30bb2d1cab5d08f669ada7
sasukeuzumaki31 <nimeshnairns@gmail.com>
2021-01-26 21:06:26 +0530

    search Update

--- public/js/searchPage.js
+++ public/js/searchPage.js
@@ -14,3 +14,16 @@ function rent(){
         document.getElementById("sale").classList.add("d-none")
     }
 }
+function mobsale(){
+    if(document.getElementById("mobsale").classList.contains("d-none")){
+        document.getElementById("mobsale").classList.remove("d-none")
+    }
+    else if(!document.getElementById("mobsale").classList.contains("d-none")){
+        document.getElementById("mobsale").classList.add("d-none")
+    }
+}
+function mobrent(){
+    if(!document.getElementById("mobsale").classList.contains("d-none")){
+        document.getElementById("mobsale").classList.add("d-none")
+    }
+}
