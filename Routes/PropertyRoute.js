7b521ab2848b9d78b739ff05a133b599702f69ca
Barath Nadar <nadar.barath18@siesgst.ac.in>
2021-01-30 03:23:11 +0530

    Added SMS services and Locked Routes

--- Routes/PropertyRoute.js
+++ Routes/PropertyRoute.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const PropertyController = require('../Controller/PropertyController');
+const AdminController = require("./../Controller/AdminController");
 const PropertyRoute = express.Router();
 var multer = require('multer');
 var upload = multer({
     storage : multer.memoryStorage()
 });
-PropertyRoute.route('/test').get(PropertyController.test);
-PropertyRoute.route('/test2').get((req,res)=>{
-    res.render('Create_property_2')
-})
-
-
-
-PropertyRoute.post('/createproperty',upload.any(),PropertyController.createProperty);
-PropertyRoute.post('/commercialproperty',upload.any(),PropertyController.CommercialProperty);
-PropertyRoute.get('/editproperty',PropertyController.EditProperty);
+PropertyRoute.get('/editproperty',AdminController.isAdmin,PropertyController.EditProperty);
 module.exports = PropertyRoute;
