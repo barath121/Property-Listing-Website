24892a9c395fd5901d9335745a49c7d792ba1206
Barath Nadar <nadar.barath18@siesgst.ac.in>
2021-08-02 15:14:06 +0530

    Shifted to Mongodb Memory Server

--- Config/database.js
+++ Config/database.js
@@ -1,28 +1,34 @@
 const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 module.exports.connect =()=>{
 new Promise(async(resolve,reject) =>{
 let DBstring = "";
-if(process.env.NODE_ENV=="test")DBstring = process.env.TEST_DATABASE;
-else if(process.env.NODE_ENV == 'prod')DBstring = process.env.PROD_DATABASE;
-else DBstring = process.env.DEV_DATABASE;
+if(process.env.NODE_ENV=="test"){
+    const mongoServer  = await MongoMemoryServer.create();
+    await mongoose.connect(mongoServer.getUri(), { useNewUrlParser: true, dbName: "santoshpropertytest", useCreateIndex: true, useUnifiedTopology: true });
+}else{
+    if(process.env.NODE_ENV == 'prod')DBstring = process.env.PROD_DATABASE;
+    else DBstring = process.env.DEV_DATABASE;
+    
+    let options = {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    };
+    mongoose.connect(DBstring,options)
+    .then(conn=>{
+        if(process.env.NODE_ENV!='test'&&process.env.NODE_ENV!='prod')
+        console.log("Connection to database sucessful");
+        resolve();
+    })
+    .catch(err=>{
+        if(process.env.NODE_ENV='test'&&process.env.NODE_ENV!='prod')
+        console.log("Connection Failed "+err );
+        reject();
+    })
+}
 
-let options = {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-};
-mongoose.connect(DBstring,options)
-.then(conn=>{
-    if(process.env.NODE_ENV!='test'&&process.env.NODE_ENV!='prod')
-    console.log("Connection to database sucessful");
-    resolve();
-})
-.catch(err=>{
-    if(process.env.NODE_ENV='test'&&process.env.NODE_ENV!='prod')
-    console.log("Connection Failed "+err );
-    reject();
-})
 })}
 module.exports.close = () =>{
 new Promise((resolve,reject) =>{
